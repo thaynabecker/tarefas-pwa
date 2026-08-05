94a81f9fca06c7f6d0b8781f53987daed984d52d
thaynabecker <thaynabecker.ifcaraquari@gmail.com>
2026-08-05 14:47:20 -0300

    feat: Unidade 8 – Notificações Push

--- vite.config.js
+++ vite.config.js
@@ -3,65 +3,18 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import { VitePWA } from 'vite-plugin-pwa';
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
     vue(),
     VitePWA({
-      registerType: 'prompt',
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
-        cleanupOutdatedCaches: true,
-        sourcemap: false,
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'google-fonts-cache',
-              expiration: {
-                maxEntries: 10,
-                maxAgeSeconds: 60 * 60 * 24 * 365, // 1 ano
-              },
-              cacheableResponse: {
-                statuses: [0, 200],
-              },
-            },
-          },
-          {
-            urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'gstatic-fonts-cache',
-              expiration: {
-                maxEntries: 10,
-                maxAgeSeconds: 60 * 60 * 24 * 365, // 1 ano
-              },
-              cacheableResponse: {
-                statuses: [0, 200],
-              },
-            },
-          },
-          {
-            urlPattern: /^http:\/\/localhost:8001\/.*/i,
-            handler: 'NetworkFirst',
-            options: {
-              cacheName: 'api-cache',
-              expiration: {
-                maxEntries: 50,
-                maxAgeSeconds: 60 * 60 * 24, // 24 horas
-              },
-              cacheableResponse: {
-                statuses: [0, 200],
-              },
-              networkTimeoutSeconds: 10,
-            },
-          },
-        ],
-      },
+      registerType: 'autoUpdate',
+      strategies: 'injectManifest',  
+      srcDir: 'src',                 
+      filename: 'sw.js',             
+
       manifest: {
-        id: 'com.tarefas-pwa',
         name: 'Gerenciador de Tarefas',
         short_name: 'Tarefas',
         description: 'Aplicativo PWA para gerenciar tarefas diárias',
@@ -70,6 +23,7 @@ export default defineConfig({
         display: 'standalone',
         scope: '/',
         start_url: '/',
+        id: 'com.task-manager.app',
         icons: [
           {
             src: '/icons/icon-192x192.png',
@@ -91,6 +45,7 @@ export default defineConfig({
       },
       devOptions: {
         enabled: true,
+        type: 'module', // 
       },
     }),
     vueDevTools(),
